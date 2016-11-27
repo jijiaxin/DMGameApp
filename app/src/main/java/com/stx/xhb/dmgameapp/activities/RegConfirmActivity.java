c865d61bcfa2660c3268f4867a46c2b97328f2cf
jijiaxin <jijiaxin@xiaomi.com>
2016-11-27 15:05:12 +0800

    个人信息

--- app/src/main/java/com/stx/xhb/dmgameapp/activities/RegConfirmActivity.java
+++ app/src/main/java/com/stx/xhb/dmgameapp/activities/RegConfirmActivity.java
@@ -32,7 +32,7 @@ public class RegConfirmActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reg2);
+        setContentView(R.layout.activity_reg_confirm);
 
         initIntent();
         initView();
